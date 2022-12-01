44f861ec6427ad4fd201c11f4751b8246327c58d
Alex Sher <sher6259@mail.ru>
2022-12-01 22:24:33 +0600

    reding and printing mouse position

--- game.py
+++ game.py
@@ -22,6 +22,8 @@ while not finished:
     pygame.display.update()
     screen.fill('Black')
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(pygame.mouse.get_pos())
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 finished = True
