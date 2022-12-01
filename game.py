22fb53ef705d831691424ed85273c4d637003bac
Alex Sher <sher6259@mail.ru>
2022-12-01 21:59:25 +0600

    I did pygame initialization

--- game.py
+++ game.py
@@ -1,4 +1,22 @@
 import pygame
 from pygame.draw import *
 from random import randint
+
 pygame.init()
+
+FPS = 2
+size = [500, 500]
+screen = pygame.display.set_mode(size)
+
+finished = False
+
+pygame.display.update()
+clock = pygame.time.Clock
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                finished = True
+                pygame.quit()
