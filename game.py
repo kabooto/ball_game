69b4a2a9f6a80f07bdc3a3a6f0359ba62d7830fe
Alex Sher <sher6259@mail.ru>
2022-12-12 20:01:41 +0600

    made the descriptions

--- game.py
+++ game.py
@@ -6,26 +6,38 @@ pygame.init()
 
 
 def new_ball():
-    global x, y
+    """
+    Makes a new ball
+    :return: None
+    """
+    global x, y  # variable for window size
     x = randint(10, 490)
     y = randint(10, 490)
-    rand_color = (randint(0, 255), randint(0, 255), randint(0, 255))
-    circle(screen, rand_color, (x, y), 10)
+    rand_color = (randint(0, 255), randint(0, 255), randint(0, 255))  # Ball colors
+    circle(screen, rand_color, (x, y), 10)  # Ball
 
 
 def click():
-    global event, score
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mouse_pos = pygame.mouse.get_pos()
-        x1, y1 = mouse_pos[0], mouse_pos[1]
-        if abs(x - x1) <= 10 and abs(y - y1) <= 10:
+    """
+    processing clicking
+    :return: None
+    """
+    global event, score  # event queue and score counter
+    if event.type == pygame.MOUSEBUTTONDOWN:  # mouse bottom down process
+        mouse_pos = pygame.mouse.get_pos()  # getting mouse click position
+        x1, y1 = mouse_pos[0], mouse_pos[1]  # separate X, Y mouse position
+        if abs(x - x1) <= 10 and abs(y - y1) <= 10:  # if mouse hit the ball
             score += 1
             print('Your score is: ', score)
 
 
 def esc_exit():
-    global finished
-    if event.type == pygame.KEYDOWN:
+    """
+    exit from the game
+    :return: None
+    """
+    global finished  # variable of working game
+    if event.type == pygame.KEYDOWN:  # condition for the finish the game
         if event.key == pygame.K_ESCAPE:
             finished = True
             pygame.quit()
@@ -33,7 +45,7 @@ def esc_exit():
 
 FPS = 1
 size = [500, 500]
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size)  # screen size
 
 finished = False
 
@@ -48,4 +60,3 @@ while not finished:
     new_ball()
     pygame.display.update()
     screen.fill('Black')
-
